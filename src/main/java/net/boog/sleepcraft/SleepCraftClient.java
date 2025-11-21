c6da483191d6397ddd19a150c9cd116f694b998e
Kristian Engel <kristian.engel@umbrage.com>
2025-11-21 12:27:09 -0700

    Update sources jar

---
+++ src/main/java/net/boog/sleepcraft/SleepCraftClient.java
@@ -0,0 +1,10 @@
+package net.boog.sleepcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SleepCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
